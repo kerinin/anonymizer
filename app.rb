1d301a55d16a8aaa4a1c425872c993cde168daca
Ryan Michael <kerinin@gmail.com>
2012-02-15 23:28:01 -0600

    hoping this will make heroku work

--- app.rb
+++ app.rb
@@ -13,7 +13,15 @@ filters_path = 'filters.txt'
 set :haml, :format => :html5
 set :haml, :escape_html => true
 
-Mongoid.load!("mongoid.yml")
+Mongoid.configure do |config|
+  if ENV['MONGOHQ_URL']
+    conn = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])
+    uri = URI.parse(ENV['MONGOHQ_URL'])
+    config.master = conn.db(uri.path.gsub(/^\//, ''))
+  else
+    config.master = Mongo::Connection.from_uri("mongodb://localhost:27017").db('anonymizer')
+  end
+end
 Mongoid.logger = Logger.new($stdout)
 #Mongoid.logger.level = 3
 
@@ -32,7 +40,7 @@ end
 
 post '/filters' do
   data = JSON.parse( request.body.read )
-  Filter.create!(:search => search, :replace => replace)
+  Filter.create!(:search => data['search'], :replace => replace)
 end
 
 get '/filters' do
--- load_db.rb
+++ load_db.rb
@@ -5,7 +5,15 @@ require "mongoid"
 require "subject"
 
 ENV['RACK_ENV'] = 'development'
-Mongoid.load!("mongoid.yml")
+Mongoid.configure do |config|
+  if ENV['MONGOHQ_URL']
+    conn = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])
+    uri = URI.parse(ENV['MONGOHQ_URL'])
+    config.master = conn.db(uri.path.gsub(/^\//, ''))
+  else
+    config.master = Mongo::Connection.from_uri("mongodb://localhost:27017").db('anonymizer')
+  end
+end
 Mongoid.logger = Logger.new($stdout)
 Mongoid.logger.level = 3
 
