1262da14549b7c6627b29b699730bca158d8cfc9
Ryan Michael <kerinin@gmail.com>
2012-02-16 07:34:04 -0600

    locking access down to @otherinbox.com users

--- app.rb
+++ app.rb
@@ -7,11 +7,14 @@ require "json"
 require "pp"
 require "mongoid"
 require "filter"
+require "oauth"
+require "gmail_xoauth"
 
 source_path = 'input.txt'
 filters_path = 'filters.txt'
 set :haml, :format => :html5
 set :haml, :escape_html => true
+enable :sessions
 
 Mongoid.configure do |config|
   if ENV['MONGOHQ_URL']
@@ -25,6 +28,47 @@ Mongoid.configure do |config|
 end
 Mongoid.logger = Logger.new($stdout)
 #Mongoid.logger.level = 3
+
+
+before do
+  session[:oauth] ||= {}  
+  
+  consumer_key = ENV["CONSUMER_KEY"] || ENV["consumer_key"] || "anonymous"
+  consumer_secret = ENV["CONSUMER_SECRET"] || ENV["consumer_secret"] || "anonymous"
+  
+  @consumer ||= OAuth::Consumer.new(consumer_key, consumer_secret,
+    :site => "https://www.google.com",
+    :request_token_path => '/accounts/OAuthGetRequestToken?scope=https://mail.google.com/%20https://www.googleapis.com/auth/userinfo%23email',
+    :access_token_path => '/accounts/OAuthGetAccessToken',
+    :authorize_path => '/accounts/OAuthAuthorizeToken'
+  )
+  
+  if !session[:oauth][:request_token].nil? && !session[:oauth][:request_token_secret].nil?
+    @request_token = OAuth::RequestToken.new(@consumer, session[:oauth][:request_token], session[:oauth][:request_token_secret])
+  end
+  
+  if !session[:oauth][:access_token].nil? && !session[:oauth][:access_token_secret].nil?
+    @access_token = OAuth::AccessToken.new(@consumer, session[:oauth][:access_token], session[:oauth][:access_token_secret])
+  end
+end
+
+before /^(?!\/(request|auth))/ do
+  redirect "/request" unless user_authorized?
+end
+
+get "/request" do
+  @request_token = @consumer.get_request_token(:oauth_callback => "#{request.scheme}://#{request.host}:#{request.port}/auth")
+  session[:oauth][:request_token] = @request_token.token
+  session[:oauth][:request_token_secret] = @request_token.secret
+  redirect @request_token.authorize_url
+end
+
+get "/auth" do
+  @access_token = @request_token.get_access_token :oauth_verifier => params[:oauth_verifier]
+  session[:oauth][:access_token] = @access_token.token
+  session[:oauth][:access_token_secret] = @access_token.secret
+  redirect "/"
+end
 
 get '/' do
   File.read(File.join('public', 'index.html'))
@@ -61,4 +105,17 @@ post '/test_filter' do
   
   content_type :json
   filter.test_results.to_json
+end
+
+private
+
+def user_authorized?
+  return false unless @access_token
+
+  response = @access_token.get('https://www.googleapis.com/userinfo/email?alt=json')
+  if response.is_a?(Net::HTTPSuccess)
+    @email = JSON.parse(response.body)['data']['email']
+  end
+
+  @email.end_with? "@otherinbox.com"
 end
